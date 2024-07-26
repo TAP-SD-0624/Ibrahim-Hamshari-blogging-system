5facdeef7d1b04b746d2a2794590ec355a1c039e
ibrahim hamshari <lomeseth1@gmail.com>
2024-07-26 20:17:22 +0300

    init

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,13 +2,17 @@ import { Sequelize } from "sequelize";
 
 const dbUsername = process.env.DBUSERNAME;
 const dbPassword = process.env.DBPASSWORD;
+const dbName = process.env.DBNAME;
+const dbHost = process.env.DBHOST;
+const dbPort = process.env.DBPORT;
 
-if (!dbUsername || !dbPassword) {
+if (!dbUsername || !dbPassword || !dbName || !dbHost || !dbPort) {
   throw new Error('Database username and/or password not set in environment variables');
 }
 
- const sequelize = new Sequelize('Blog', dbUsername, dbPassword, {
-  host: 'localhost',
+ const sequelize = new Sequelize(dbName, dbUsername, dbPassword, {
+  host: dbHost,
+  port: parseInt(dbPort),
   dialect: 'mysql',
   logging:false
 });
